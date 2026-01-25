4b9f643a8fbbedf748c791efffa867bde1c2ce0d
MrBanana <bananaman3264@gmail.com>
2026-01-25 16:19:34 -0600

    Modified Scratch block storage structure

--- src/main.c
+++ src/main.c
@@ -4,69 +4,56 @@
 #include <stdbool.h>
 #include "std_vector.h"
 
-typedef struct ScratchLine 
+struct json_object* blocks;
+
+#define AsUnmanagedString(a) (String){false, a}
+#define AsManagedString(a)   (String){true,  a}
+
+#define freeIfUnmanaged(a) if(!a.managed) { free(a.data); }
+
+enum ArgTypes
 {
-	char* opcode;
+	ArgType_Pointer = 3,
+	ArgType_Text	= 4,
+	ArgType_Number	= 5,
+	ArgType_Number1	= 6,	// Positive number | These two are Scratch jankness; they
+	ArgType_Number2	= 7,	// Angle		   | work the same way as ArgType_Number
+};
+
+typedef struct String
+{
+	bool managed; // True if the string should not be freed (Managed by json-c)
+	char* data;
+} String;
+
+typedef union ScratchArgData 
+{
+	String idPointer;
+	String text;
+	double number;
+} ScratchArgData;
+
+typedef struct ScratchBlock 
+{
+	String id;
+	String opcode;
+	String next;
+	String previous;
 	int args;
-	char** argsData;
-	int indentation;
-} ScratchLine;
+	int* argtypes;
+	ScratchArgData* argdata;
+} ScratchBlock;
 
-struct vecScratchLine {
-	ScratchLine* data; size_t allocated_size; size_t length; size_t sizeoftype;
+struct vecScratchBlock {
+	ScratchBlock* data; size_t allocated_size; size_t length; size_t sizeoftype;
 } lines;
-
-void PrintScratchLine(ScratchLine line)
-{
-	printf("%s(",line.opcode);
-	for (int i = 0; i < line.args - 1; i++) 
-	{
-		printf("%s,",line.argsData[i]);
-	}
-	if (line.args != 0)
-	{
-		printf("%s", line.argsData[line.args - 1]);
-	}
-	printf(");\n");
-}
-
-char* GetArgFromJson(struct json_object* arg) 
-{
-	//printf("%s\n", json_object_to_json_string(json_object_array_get_idx(json_object_array_get_idx(arg, 1), 1)));
-
-	return json_object_to_json_string(json_object_array_get_idx(json_object_array_get_idx(arg, 1), 1));
-}
-
-ScratchLine GetScratchLineFromJson(struct json_object* object)
-{
-	ScratchLine sl;
-	sl.opcode = json_object_get_string(json_object_object_get(object, "opcode"));
-
-	struct json_object* args = json_object_object_get(object, "inputs");
-
-	sl.args = json_object_object_length(args);
-
-	sl.argsData = malloc(sl.args); if (!sl.argsData) { printf("Malloc error!"); exit(-1); }
-
-	int i = 0;
-
-	json_object_object_foreach(args, key, val)
-	{
-		sl.argsData[i] = GetArgFromJson(val); i++;
-	}
-
-
-
-	return sl;
-}
 
 int main()
 {
-	 
-	lines.allocated_size = 8; lines.length = 0; lines.sizeoftype = sizeof(ScratchLine); lines.data = malloc(sizeof(ScratchLine) * lines.allocated_size); if (!lines.data) {
+	lines.allocated_size = 8; lines.length = 0; lines.sizeoftype = sizeof(ScratchBlock); lines.data = malloc(sizeof(ScratchBlock) * lines.allocated_size); if (!lines.data) {
 		exit(-1);
 	}
-	
+
 	FILE* json = fopen("../../../../scratch/Project/project.json", "r");
 	if (json == NULL)
 	{
@@ -101,32 +88,88 @@ int main()
 
 	struct json_object* project = json_tokener_parse(text);
 
-	struct json_object* blocks = json_object_object_get(json_object_array_get_idx(json_object_object_get(project, "targets"), 1), "blocks");
-	
-	char* next = NULL;
+	blocks = json_object_object_get(json_object_array_get_idx(json_object_object_get(project, "targets"), 1), "blocks");
 
 	json_object_object_foreach(blocks, key, val)
 	{
-		if (strcmp(json_object_get_string(json_object_object_get(val, "opcode")),"event_whenflagclicked") == 0)
+		ScratchBlock sb;
+		sb.id = AsManagedString(key);
+		struct json_object* next = json_object_object_get(val, "next");
+		if (json_object_is_type(next, json_type_null)) 
 		{
-			next = json_object_get_string(json_object_object_get(val, "next"));
+			sb.next = (String){ NULL, false };
 		}
-	}
+		else
+		{
+			sb.next = AsManagedString(json_object_get_string(next));
+		}
+		struct json_object* previous = json_object_object_get(val, "parent");
+		if (json_object_is_type(previous, json_type_null))
+		{
+			sb.previous = (String){ NULL, false };
+		}
+		else
+		{
+			sb.previous = AsManagedString(json_object_get_string(previous));
+		}
 
-	while (json_object_get_string(json_object_object_get(json_object_object_get(blocks, next), "next")) != NULL)
-	{
-		std_vector_pushback(lines, GetScratchLineFromJson(json_object_object_get(blocks, next)));
+		sb.opcode = AsManagedString(json_object_get_string(json_object_object_get(val,"opcode")));
 
-		next = json_object_get_string(json_object_object_get(json_object_object_get(blocks, next), "next"));
+		sb.args = json_object_object_length(json_object_object_get(val, "inputs"));
+
+		sb.argdata  = malloc(sb.args * sizeof(ScratchArgData));
+		sb.argtypes = malloc(sb.args * sizeof(int)			 );
+
+		int i = 0;
+		json_object_object_foreach(json_object_object_get(val, "inputs"), block_key, block_val)
+		{
+			int a = json_object_get_int(json_object_array_get_idx(block_val, 0));
+			struct json_object* this = block_val;
+			sb.argtypes[i] = a;
+			get_by_type:
+			switch (a) 
+			{
+			case 1:
+			case 2:
+				a = json_object_get_int(json_object_array_get_idx(json_object_array_get_idx(this, 1),0));
+				this = json_object_array_get_idx(this, 1);
+				goto get_by_type;
+			case ArgType_Pointer:
+				sb.argdata[i].idPointer = AsManagedString(json_object_get_string(json_object_array_get_idx(this, 1)));
+				sb.argtypes[i] = a;
+				break;
+			case ArgType_Text:
+				sb.argdata[i].text = AsManagedString(json_object_get_string(json_object_array_get_idx(this, 1)));
+				sb.argtypes[i] = a;
+				break;
+			default:
+				printf("These argument types have not been implemented!");
+				exit(-1);
+			}
+			i++;
+		}
+
+		std_vector_pushback(lines, sb);
 	}
-	std_vector_pushback(lines, GetScratchLineFromJson(json_object_object_get(blocks, next)));
 
 	for (int i = 0; i < lines.length; i++) 
 	{
-		PrintScratchLine(lines.data[i]);
+		ScratchBlock sb = lines.data[i];
+		printf("Id: %s\nOpcode: %s\nNext: %s\nParent: %s\n", sb.id.data, sb.opcode.data, sb.next.data, sb.previous.data);
+		for (int j = 0; j < sb.args; j++)
+		{
+			switch (sb.argtypes[j])
+			{
+			case ArgType_Pointer:
+				printf("\tBlock pointer: %s\n", sb.argdata[j].text.data);
+				break;
+			case ArgType_Text:
+				printf("\tString: %s\n", sb.argdata[j].text.data);
+				break;
+			}
+		}
+		printf("\n");
 	}
-
-	free(text);
 
 	return 0;
 }
